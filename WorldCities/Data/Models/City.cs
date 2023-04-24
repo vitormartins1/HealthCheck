16bc1894fe4b13f1861b49f36df0b24a1bda82c4
Vitor Martins <vitormartins.one@gmail.com>
2023-04-23 21:21:31 -0300

    adicionando entidades city e country

---
+++ WorldCities/Data/Models/City.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class City
+    {
+        public City()
+        {
+
+        }
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Name_ASCII { get; set; }
+
+        public decimal Lat { get; set; }
+
+        public decimal Lon { get; set; }
+
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+
+        public virtual Country Country { get; set; }
+    }
+}
---
+++ WorldCities/Data/Models/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+
+        }
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ISO2 { get; set; }
+
+        public string ISO3 { get; set; }
+
+        public virtual List<City> Cities { get; set; }
+    }
+}
