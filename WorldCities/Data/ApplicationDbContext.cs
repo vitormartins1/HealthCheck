f70cf95c0e4f42e52d9dd7e63ec8ba2fd21b76b9
Vitor Martins <vitormartins.one@gmail.com>
2023-04-24 22:25:32 -0300

    adicionando dbContext

---
+++ WorldCities/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCities.Data.Models;
+
+namespace WorldCities.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base() { }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
--- WorldCities/Startup.cs
+++ WorldCities/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WorldCities.Data;
 
 namespace WorldCities
 {
@@ -26,6 +28,9 @@ namespace WorldCities
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
