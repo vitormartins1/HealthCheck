0828cf7de242f29553818e01797d61bf44779676
Vitor Martins <vitormartins.one@gmail.com>
2023-04-29 02:25:51 -0300

    countries

--- WorldCities/ClientApp/src/app/app.module.ts
+++ WorldCities/ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CitiesComponent } from './cities/cities.component';
+import { CountriesComponent } from './countries/countries.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 
@@ -17,7 +18,8 @@ import { AngularMaterialModule } from './angular-material.module';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CitiesComponent
+    CitiesComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
--- WorldCities/Data/Models/Country.cs
+++ WorldCities/Data/Models/Country.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WorldCities.Data.Models
@@ -21,8 +22,10 @@ namespace WorldCities.Data.Models
 
         public string Name { get; set; }
 
+        [JsonPropertyName("iso2")]
         public string ISO2 { get; set; }
 
+        [JsonPropertyName("iso3")]
         public string ISO3 { get; set; }
 
         public virtual List<City> Cities { get; set; }
