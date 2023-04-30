3a72f5653c427c9187ce397504ffcf294e11aa92
Vitor Martins <vitormartins.one@gmail.com>
2023-04-29 21:15:59 -0300

    model-driven client-side validation

--- WorldCities/ClientApp/src/app/cities/city-edit.component.ts
+++ WorldCities/ClientApp/src/app/cities/city-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { City } from './City';
 import { Country } from './../countries/Country';
@@ -27,10 +27,10 @@ export class CityEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      name: new FormControl(''),
-      lat: new FormControl(''),
-      lon: new FormControl(''),
-      countryId: new FormControl('')
+      name: new FormControl('', Validators.required),
+      lat: new FormControl('', Validators.required),
+      lon: new FormControl('', Validators.required),
+      countryId: new FormControl('', Validators.required)
     });
 
     this.loadData();
