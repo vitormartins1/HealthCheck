cd4d17cc4de731e6b4128b6578c1580714655dda
Vitor Martins <vitormartins.one@gmail.com>
2023-05-01 15:27:20 -0300

    entity dto

---
+++ WorldCities/Data/CityDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data
+{
+    public class CityDto
+    {
+        public CityDto() { }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Name_ASCII { get; set; }
+
+        public decimal Lat { get; set; }
+
+        public decimal Lon { get; set; }
+
+        public int CountryId { get; set; }
+
+        public string CountryName { get; set; }
+    }
+}
---
+++ WorldCities/Data/CountryDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data
+{
+    public class CountryDto
+    {
+        public CountryDto()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [JsonPropertyName("iso2")]
+        public string ISO2 { get; set; }
+        [JsonPropertyName("iso3")]
+        public string ISO3 { get; set; }
+        public int TotCities { get; set; }
+    }
+}
