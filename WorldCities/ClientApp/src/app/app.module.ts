415c646f52fb0b77db41a2cc46efa0bebeb721bd
Vitor Martins <vitormartins.one@gmail.com>
2023-04-30 23:03:50 -0300

    add/edit country

--- WorldCities/ClientApp/src/app/app.module.ts
+++ WorldCities/ClientApp/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { CitiesComponent } from './cities/cities.component';
 import { CityEditComponent } from './cities/city-edit.component';
 import { CountriesComponent } from './countries/countries.component';
+import { CountryEditComponent } from './countries/country-edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -22,7 +23,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     HomeComponent,
     CitiesComponent,
     CityEditComponent,
-    CountriesComponent
+    CountriesComponent,
+    CountryEditComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
--- WorldCities/Controllers/CountriesController.cs
+++ WorldCities/Controllers/CountriesController.cs
@@ -117,5 +117,28 @@ namespace WorldCities.Controllers
         {
             return _context.Countries.Any(e => e.Id == id);
         }
+
+        [HttpPost]
+        [Route("IsDupeField")]
+        public bool IsDupeField(
+            int countryId,
+            string fieldName,
+            string fieldValue)
+        {
+            switch (fieldName)
+            {
+                case "name":
+                    return _context.Countries.Any(
+                        c => c.Name == fieldValue && c.Id != countryId);
+                case "iso2":
+                    return _context.Countries.Any(
+                        c => c.ISO2 == fieldValue && c.Id != countryId);
+                case "iso3":
+                    return _context.Countries.Any(
+                        c => c.ISO3 == fieldValue && c.Id != countryId);
+                default:
+                    return false;
+            }
+        }
     }
 }
