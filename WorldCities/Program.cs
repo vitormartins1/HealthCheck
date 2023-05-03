1d726216c471ee0920fee0ede6ff85aade3c96e0
Vitor Martins <vitormartins.one@gmail.com>
2023-05-02 21:58:57 -0300

    serilog

--- WorldCities/Program.cs
+++ WorldCities/Program.cs
@@ -16,6 +16,37 @@ namespace WorldCities
             CreateHostBuilder(args).Build().Run();
         }
 
+        //public static void Main(string[] args)
+        //{
+        //    var configuration = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("appsettings.json",
+        //            optional: false,
+        //            reloadOnChange: true)
+        //        .AddJsonFile(string.Format("appsettings.{0}.json",
+        //            Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+        //                ?? "Production"),
+        //            optional: true,
+        //            reloadOnChange: true)
+        //        .AddUserSecrets<Startup>(optional: true, reloadOnChange: true)
+        //        .Build();
+
+        //    Log.Logger = new LoggerConfiguration()
+        //        .WriteTo.MSSqlServer(
+        //            connectionString:
+        //                configuration.GetConnectionString("DefaultConnection"),
+        //            restrictedToMinimumLevel: LogEventLevel.Information,
+        //            sinkOptions: new MSSqlServerSinkOptions
+        //            {
+        //                TableName = "LogEvents",
+        //                AutoCreateSqlTable = true
+        //            })
+        //        .WriteTo.Console()
+        //        .CreateLogger();
+
+        //    CreateHostBuilder(args).UseSerilog().Build().Run();
+        //}
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
